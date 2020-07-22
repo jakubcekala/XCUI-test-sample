04b632124b0d3057bbc09eec1a2963f0c3393a38
Jakub <jakubcekala1@gmail.com>
2020-07-22 20:20:17 +0200

    Create CustomIntervalFragmentScreen

---
+++ ToDoListUITests/Screens/Tasks/CustomIntervalFragmentScreen.swift
@@ -0,0 +1,21 @@
+//
+//  CustomIntervalFragmentScreen.swift
+//  ToDoListUITests
+//
+//  Created by Jakub on 22/07/2020.
+//  Copyright © 2020 Radu Ursache - RanduSoft. All rights reserved.
+//
+
+import XCTest
+
+class CustomIntervalFragmentScreen: TasksFragment {
+    
+    let fragmentName = "Custom Interval"
+    
+    override init() {
+        super.init()
+        if(!application.navigationBars[fragmentName].staticTexts[fragmentName].exists){
+            XCTAssertThrowsError(fragmentName + " is not available")
+        }
+    }
+}
