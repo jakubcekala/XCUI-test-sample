3bf606749b44e54157c89c6c26f7a95c89ee481b
Jakub <jakubcekala1@gmail.com>
2020-07-29 19:40:45 +0200

    Implements ThemeScreen

--- ToDoListUITests/Screens/ThemesScreen.swift
+++ ToDoListUITests/Screens/ThemesScreen.swift
@@ -6,8 +6,46 @@
 //  Copyright © 2020 Radu Ursache - RanduSoft. All rights reserved.
 //
 
-import Foundation
+import XCTest
 
-class ThemesScreen {
+class ThemesScreen: PageObject {
     
+    let application = XCUIApplication()
+    
+    let settingsButton: XCUIElement
+    let colorsTiles: XCUIElementQuery
+    let okAlertDialog: XCUIElement
+    
+    override init() {
+        settingsButton = application.navigationBars["Themes"].buttons["Settings"]
+        colorsTiles = application.tables.staticTexts
+        okAlertDialog = application.alerts.scrollViews.otherElements.buttons["OK"]
+    }
+    
+    func clickOnColorButton(theme: ThemeType) {
+        switch theme {
+        case .alizarinRed:
+            colorsTiles["Alizarin Red"].tap()
+        case .vanadylBlue:
+            colorsTiles["Vanadyl Blue"].tap()
+        case .skirretGreen:
+            colorsTiles["Skirret Green"].tap()
+        case .sunflowerYellow:
+            colorsTiles["Sunflower Yellow"].tap()
+        case .rediantOrange:
+            colorsTiles["Radiant Orange"].tap()
+        case .rosePink:
+            colorsTiles["Rose Pink"].tap()
+        case .midnightBlack:
+            colorsTiles["Midnight Black"].tap()
+        }
+    }
+    
+    func clickOnOkAlertDialog() {
+        okAlertDialog.tap()
+    }
+
+    enum ThemeType {
+        case alizarinRed, vanadylBlue, skirretGreen, sunflowerYellow, rediantOrange, rosePink, midnightBlack
+    }
 }
